08536d9c55a9cf36aceb8636847e43c0cb302bf8
Leonam-Comercial <leonam.comseg@gmail.com>
2024-08-21 16:41:13 -0300

    Carlos

--- autenticacao/models.py
+++ autenticacao/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Cadastro(models.Model):
+  username = models.CharField(max_length=50)
+  password = models.IntegerField()
+  confirm_password = models.IntegerField()
+
+  def __str__(self):
+    return self.username
+  
+
+
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.messages import constants
+from django.contrib import messages
+from .models import Cadastro
 
 def cadastro(request):
-  return render(request, 'cadastro.html')
+  if request.method == 'GET':
+    return render(request, 'cadastro.html')
+
+  elif request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    confirm_password = request.POST.get('confirm_password')
+
+    if (len(username.strip())==0) or (len(password.strip())==0) or (len(confirm_password.strip())==0):
+      messages.add_message(request, constants.ERROR, "Preencha todos os campos!")
+      return redirect('cadastro')
+    if password != confirm_password:
+      messages.add_message(request, constants.ERROR, 'As senhas não conferem!')
+      return redirect('cadastro')
+    
+    novo_usuario = Cadastro(
+      username = username,
+      password = password,
+      confirm_password = confirm_password
+    )
+    # novo_usuario.save()
+    
+    messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso...faça o login para entrar!')
+    return redirect('logar')
 
 def logar(request):
+  if request.method == 'GET':
+    return render(request, 'login.html')
+
   return HttpResponse('login')
