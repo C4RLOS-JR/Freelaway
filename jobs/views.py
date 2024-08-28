59edb3cc4ac25e99ba8e1e3024db238e0e335a5d
C4RLOS-JR <camjr3z@gmail.com>
2024-08-28 20:17:22 -0300

    Carlos

--- jobs/views.py
+++ jobs/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render, redirect
 from .models import Jobs
 
 def encontrar_jobs(request):
-  preco_minimo = request.POST.get('preco_minimo')
-  preco_maximo = request.POST.get('preco_maximo')
-  prazo_minimo = request.POST.get('prazo_minimo')
-  prazo_maximo = request.POST.get('prazo_maximo')
-  categoria = request.POST.get('categoria')
-  jobs = Jobs.objects.all()
+  preco_minimo = request.GET.get('preco_minimo')
+  preco_maximo = request.GET.get('preco_maximo')
+  prazo_minimo = request.GET.get('prazo_minimo')
+  prazo_maximo = request.GET.get('prazo_maximo')
+  categoria = request.GET.get('categoria')
+  jobs = Jobs.objects.filter(reservado=False)
 
   if preco_minimo:
     jobs = jobs.filter(preco__gte=preco_minimo)
   if preco_maximo:
-    jobs = jobs.filter(preco__lte=preco_minimo)
+    jobs = jobs.filter(preco__lte=preco_maximo)
   if prazo_minimo:
     jobs = jobs.filter(prazo_entrega__gte=prazo_minimo)
   if prazo_maximo:
